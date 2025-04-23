3f455bbb7dba058d9af300cf675d33f3679878f7
ArcVaw <94964623+archievawser@users.noreply.github.com>
2025-04-24 08:12:34 +0930

    riot push #1

--- Source/Program.cs
+++ Source/Program.cs
@@ -1,12 +1,7 @@
 ﻿using Rabid;
 using System;
 
-/* ---------- Terminology  -----------
- * Service - a static manager class
- * Manager - manages other class instances
- * Unbound Rpc - a set of RPC functions on a static class
- * 
- */
+
 class Program
 {
 	static void Main(string[] args)
